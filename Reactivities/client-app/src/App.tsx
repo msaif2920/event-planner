5efb5b88d6488507942b69a316d22563347be48c
samin <saminsaif29@gamil.com>
2023-02-06 23:00:38 -0500

    setup of client side

---
+++ Reactivities/client-app/src/App.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { Header } from 'semantic-ui-react';
+import List from 'semantic-ui-react/dist/commonjs/elements/List';
+import Button from 'semantic-ui-react/dist/commonjs/elements/Button';
+
+function App() {
+
+  const [activitites, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+    .then(resp => setActivities(resp.data));
+  }, []);
+
+  return (
+    <div className="App">
+      <Header as='h2' icon='users' content='Reactivities'/>
+        <List>
+          {
+            activitites.map((activity: any) => (
+              <List.Item key = {activity.id}>
+                {activity.title}
+              </List.Item>
+            ))
+          }
+        </List>
+        <Button content={'TEST'}/>
+    </div>
+  );
+}
+
+export default App;
